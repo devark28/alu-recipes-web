69ad35f4e2a4983f99b8b774f5be370b0233e183
devark28 <e.shimwa1@alustudent.com>
2025-03-28 14:34:25 +0200

    UPD: implement cicd webhook server

---
+++ py_cicd_script.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import subprocess
+
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        if self.path == '/':
+            try:
+                # Execute the bash script
+                result = subprocess.run(['/bin/bash', 'cicd_pull_script.sh'], capture_output=True, text=True, check=True)
+                self.send_response(200)
+                self.send_header('Content-type', 'text/plain')
+                self.end_headers()
+                self.wfile.write(result.stdout.encode())
+
+            except subprocess.CalledProcessError as e:
+                self.send_response(500)
+                self.send_header('Content-type', 'text/plain')
+                self.end_headers()
+                self.wfile.write(b"Error executing script.\n")
+                print(f"Error executing script: {e.stderr}", end="")
+
+            except FileNotFoundError:
+                self.send_response(404)
+                self.send_header('Content-type', 'text/plain')
+                self.end_headers()
+                self.wfile.write(b"Script not found.\n")
+
+            except Exception as e:
+                self.send_response(500)
+                self.send_header('Content-type', 'text/plain')
+                self.end_headers()
+                self.wfile.write(b"Internal server error.")
+                print(f"Internal server error: {e}")
+
+        else:
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b"Resource not found.\n")
+            print(f"Resource not Found: /{self.path}")
+
+def run(server_class=HTTPServer, handler_class=SimpleHandler, port=8080):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting server on port {port}')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("\nKeyboard interrupt received, shutting down the server.")
+        httpd.server_close() 
+        exit(0) 
+    except Exception as e:
+        print("found one you didn't fix!")
+
+if __name__ == "__main__":
+    run()
